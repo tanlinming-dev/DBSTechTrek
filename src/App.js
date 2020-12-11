21536dad40c7161ec8f3b019be86b578cf1e9734
Lim Jing Zhi <limjingzhi.cs@gmail.com>
2020-12-11 14:25:49 +0800

    ui for view balance

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
 import './App.css';
 import Navbar from './Components/Navbar';
 import Home from './Components/Home';
-<<<<<<< HEAD
-=======
 import ViewBalance from './Components/ViewBalance';
->>>>>>> navbar
 import TransactionHistory from './Components/TransactionHistory';
 import AddTransaction from './Components/AddTransaction';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
@@ -15,7 +12,7 @@ function App() {
     <div className="App">
         <Navbar />
         <Switch>
-          <Route path="/" exact component={ViewBalance} />
+          <Route path="/ViewBalance" exact component={ViewBalance} />
           <Route path="/AddTransaction" component={AddTransaction} />
           <Route path="/TransactionHistory" component={TransactionHistory} />
         </Switch>
--- src/Components/ViewBalance.js
+++ src/Components/ViewBalance.js
@@ -1,12 +1,62 @@
-import '../App.css';
+import React from 'react';
+import Table from 'react-bootstrap/Table';
+import axios from 'axios';
 
-function ViewBalance() {
-    return (
-      <div className="App">
-  
-      </div>
-    );
+class ViewBalance extends React.Component {
+  state = {
+    accounts: [],
   }
-  
-  export default ViewBalance;
-  
+
+  componentDidMount() {
+
+    const config = {
+      headers: {
+        'x-api-key': 'ttP2UvBYv87loHrose0BX6WnjdqGwRl78QQMtuYy',
+      }
+    };
+
+    const info = {
+      'custID': 17
+    }
+
+    axios.post(`https://u8fpqfk2d4.execute-api.ap-southeast-1.amazonaws.com/techtrek2020/accounts/view`,
+      info, config)
+      .then(res => {
+        console.log(res.data)
+        const accounts = res.data;
+        this.setState({ accounts });
+        console.log(accounts);
+      })
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Account Details</h2>
+        <Table striped bordered hover variant="dark">
+          <thead>
+            <tr>
+              <th>Account Name</th>
+              <th>Account Number</th>
+              <th>Available Balance</th>
+              <th>Is Linked?</th>
+            </tr>
+          </thead>
+          <tbody>
+          { this.state.accounts.filter(account => account.custID === 17).map(account => 
+            <tr key="{accounts.accountName}">
+              <td>{account.accountName}</td>
+              <td>{account.accountNumber}</td>
+              <td>{account.availableBal}</td>
+              <td>{account.linked}</td>
+            </tr>
+            )
+          }
+          </tbody>
+        </Table>
+      </div>
+    )
+  }
+}
+
+export default ViewBalance;
