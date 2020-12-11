c457eab6d4b3ffac62f7507d9659c4dcf5ad5392
Lim Jing Zhi <limjingzhi.cs@gmail.com>
2020-12-11 15:10:16 +0800

    propagate data for view all account balance

--- src/Components/ViewBalance.js
+++ src/Components/ViewBalance.js
@@ -3,31 +3,55 @@ import Table from 'react-bootstrap/Table';
 import axios from 'axios';
 
 class ViewBalance extends React.Component {
-  state = {
-    accounts: [],
-  }
-
-  componentDidMount() {
-
-    const config = {
-      headers: {
-        'x-api-key': 'ttP2UvBYv87loHrose0BX6WnjdqGwRl78QQMtuYy',
-      }
-    };
-
-    const info = {
-      'custID': 17
+    constructor(props) {
+      super(props);
+      this.state = {
+        custID: 7,
+        payeeID: '',
+        dateTime: new Date(),
+        amount: 0.0,
+        expensesCat: '',
+        eGift: false,
+        message: '',
+        payeeIDError: '',
+        dateTimeError: '',
+        amountError: '',
+        expensesCatError: '',
+        messageError: '',
+        balanceAmount: 0,
+        payees: {},
+        error: "",
+        accounts:[],
+      };
     }
 
-    axios.post(`https://u8fpqfk2d4.execute-api.ap-southeast-1.amazonaws.com/techtrek2020/accounts/view`,
-      info, config)
-      .then(res => {
-        console.log(res.data)
-        const accounts = res.data;
-        this.setState({ accounts });
-        console.log(accounts);
-      })
+  componentDidMount() {
+    this.checkAmountBalance();
   }
+
+  checkAmountBalance() {
+		var data = JSON.stringify({
+			custID: this.state.custID,
+		});
+		axios({
+        method: 'post',
+        url: 'https://u8fpqfk2d4.execute-api.ap-southeast-1.amazonaws.com/techtrek2020/accounts/view',
+        headers: {
+          'x-api-key': 'ttP2UvBYv87loHrose0BX6WnjdqGwRl78QQMtuYy',
+          'Content-Type': 'application/json',
+        },
+        data: data,
+      })
+        .then((response) => response)
+        .catch((error) => error.response)
+        .then((response) => {
+          if (response.status === '200' || response.status === 200) {
+            let accounts = response.data;
+            this.setState({ accounts: accounts });
+            console.log(accounts)
+          }
+        });
+    }
 
   render() {
     return (
@@ -36,19 +60,19 @@ class ViewBalance extends React.Component {
         <Table striped bordered hover variant="dark">
           <thead>
             <tr>
-              <th>Account Name</th>
-              <th>Account Number</th>
-              <th>Available Balance</th>
-              <th>Is Linked?</th>
+              <th><h5>Account Name</h5></th>
+              <th><h5>Account Number</h5></th>
+              <th><h5>Available Balance</h5></th>
+              <th><h5>Is Linked?</h5></th>
             </tr>
           </thead>
           <tbody>
-          { this.state.accounts.filter(account => account.custID === 17).map(account => 
+          { this.state.accounts.map(account => 
             <tr key="{accounts.accountName}">
               <td>{account.accountName}</td>
               <td>{account.accountNumber}</td>
-              <td>{account.availableBal}</td>
-              <td>{account.linked}</td>
+              <td>SGD ${account.availableBal}</td>
+              <td>{(account.linked).toString()}</td>
             </tr>
             )
           }
@@ -60,3 +84,5 @@ class ViewBalance extends React.Component {
 }
 
 export default ViewBalance;
+
+
