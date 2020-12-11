f662cd9844161e45bd2a0598cd3557a2b0a6e932
pratyayj <pratyay.jaidev@gmail.com>
2020-12-11 12:09:29 +0800

    move TransactionHistory to Components

--- src/Components/TransactionHistory.js
+++ src/Components/TransactionHistory.js
@@ -1,9 +1,35 @@
-function App() {
-  return (
-    <div className="App">
+import React from 'react';
 
-    </div>
-  );
+import axios from 'axios';
+
+export default class TransactionHistory extends React.Component {
+  state = {
+    transactions: []
+  }
+
+  componentDidMount() {
+
+  const config = {
+    headers: {
+      'x-api-key': 'ttP2UvBYv87loHrose0BX6WnjdqGwRl78QQMtuYy',
+    }
+  };
+
+    const info = {
+      'custId': 17
+    }
+
+    axios.post(`https://u8fpqfk2d4.execute-api.ap-southeast-1.amazonaws.com/techtrek2020/transaction/view`,
+      info, config)
+      .then(res => {
+        const transactions = res.data;
+        this.setState({ transactions });
+      })
+  }
+
+  render() {
+    return (
+      <h1>temp</h1>
+    )
+  }
 }
-
-export default App;
