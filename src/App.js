f8623aa9ebb26b94ce9176f715d9154e673d07af
Fong Kin Shing <ksfong.2017@sis.smu.edu.sg>
2020-12-11 16:09:17 +0800

    Add Functionality to prevent loggedin user to go to login page

--- src/App.js
+++ src/App.js
@@ -4,33 +4,42 @@ import TransactionHistory from './Components/TransactionHistory';
 import AddTransaction from './Components/AddTransaction';
 import Loginpage from './Components/Loginpage';
 import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 import PrivateRoute from './Components/PrivateRoute';
 // export const AuthContext = React.createContext(null);
 
 function App() {
+	// const [isLoggedIn, setLoggedIn] = useState(false);
+	const isLogin = () => {
+		return sessionStorage.getItem('isLoggedIn') === 'true' ? true : false;
+	};
 
-  // const [isLoggedIn, setLoggedIn] = useState(false);
-  const isLogin = () => { 
-    return sessionStorage.getItem('isLoggedIn') === 'true' ? true: false;
-}
+	let routes;
 
-  let routes;
-
-    routes = (
-      <Switch>
-        <PrivateRoute path="/home" component={Home} />
-        <PrivateRoute path="/AddTransaction" component={AddTransaction} />
-        <PrivateRoute path="/TransactionHistory" component={TransactionHistory} />
-        <Route exact path='/login' component={Loginpage} />
-        {!isLogin ? <Route exact path="/">
-          <Redirect to="/login" />
-        </Route> : <Route exact path="/">
-          <Redirect to="/home" />
-    </Route> }
-      </Switch>
-    );
-    /*
+	routes = (
+		<Switch>
+			<PrivateRoute path="/home" component={Home} />
+			<PrivateRoute path="/AddTransaction" component={AddTransaction} />
+			<PrivateRoute path="/TransactionHistory" component={TransactionHistory} />
+			{isLogin ? (
+				<Route exact path="/login">
+					<Redirect to="/home" />
+				</Route>
+			) : (
+				<Route exact path="/login" component={Loginpage} />
+			)}
+			{!isLogin ? (
+				<Route exact path="/">
+					<Redirect to="/login" />
+				</Route>
+			) : (
+				<Route exact path="/">
+					<Redirect to="/home" />
+				</Route>
+			)}
+		</Switch>
+	);
+	/*
   } else {
     routes = (
       <Switch>
@@ -42,15 +51,13 @@ function App() {
     )
   }
 */
-  return (
-    // <AuthContext.Provider value={{ isLoggedIn, setLoggedIn }}>
-    <div className="App">
-      <Router>
-        {routes}
-      </Router>
-    </div>
-    // </AuthContext.Provider >
-  );
+	return (
+		// <AuthContext.Provider value={{ isLoggedIn, setLoggedIn }}>
+		<div className="App">
+			<Router>{routes}</Router>
+		</div>
+		// </AuthContext.Provider >
+	);
 }
 
 export default App;
